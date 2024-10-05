97363d69c563b72b51a26db3e9bcc60de978c7c9
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-10-05 13:32:42 +0200

    gateway commit

---
+++ src/main/java/com/freddan/mediaproject_gateway/MediaprojectGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.freddan.mediaproject_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaprojectGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaprojectGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freddan/mediaproject_gateway/MediaprojectGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freddan.mediaproject_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaprojectGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
